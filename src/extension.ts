82a9491ed840aa47460a8f688f78ac6329b4110e
Johannes Rieken <johannes.rieken@gmail.com>
2021-01-25 12:14:47 +0100

    add slow case

--- src/extension.ts
+++ src/extension.ts
@@ -10,7 +10,12 @@ export function activate(context: vscode.ExtensionContext) {
 
 			const edit = new vscode.WorkspaceEdit();
 			edit.insert(first.oldUri, new vscode.Position(0, 0), `MOVE from ${first.oldUri.toString()} to ${first.newUri.toString()}\n`);
-			e.waitUntil(Promise.resolve(edit));
+
+			let wait = Promise.resolve(undefined);
+			if (first.oldUri.path.includes('slow')) {
+				wait = new Promise(resolve => setTimeout(resolve, 5678));
+			}
+			e.waitUntil(wait.then(() => edit));
 		}
 	});
 }
